2201723fea01c46752164380fd73bb2e0714c2f7
unknown <liuliang1211@163.com>
2018-05-06 02:02:49 +0800

    first commit

---
+++ src/main/java/com/leon/HelloController.java
@@ -0,0 +1,21 @@
+package com.leon;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+
+	@RequestMapping("/getDemo")
+	public Demo getDemo() {
+		Demo demo = new Demo();
+		demo.setId(1);
+		demo.setName("liuliang");
+		return demo;
+	}
+}
